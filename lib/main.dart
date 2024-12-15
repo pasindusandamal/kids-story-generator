c4961c94087a7161d773a40a15569aee8f64ff89
pasindusandamal <pkpsandamal25@gmail.com>
2024-12-15 12:55:32 +0530

    Refactor main entry point to use ChangeNotifierProvider and update home screen to StoryGenerator

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:kids_story_generator/controller/story_controller.dart';
 import 'package:kids_story_generator/view/story_generator.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(const MainApp());
@@ -10,8 +12,13 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: SimpleStoryScreen(),
-    );
+    return ChangeNotifierProvider(create: (_) =>StoryController(),
+    child: MaterialApp(
+      title: 'Story Generator',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),home: const StoryGenerator(),
+    ),);
   }
 }
